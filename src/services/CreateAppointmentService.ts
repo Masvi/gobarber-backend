e4de9039e27e440c449294e54f426c8e8206828f
masvi <jeanmasvi@gmail.com>
2020-04-29 19:04:17 -0300

    creating service and reducing code of the route

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import { startOfHour } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+import Appointment from '../models/Appoitment';
+
+interface RequestDTO {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ provider, date }: RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentsInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentsInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
