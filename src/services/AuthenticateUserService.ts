53632d1167b14f1b7ec0a4529ff0cf522919863c
masvi <jeanmasvi@gmail.com>
2020-05-09 16:13:50 -0300

    global handle err

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -4,6 +4,7 @@ import { getRepository } from 'typeorm';
 
 import User from '../models/User';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 interface Resquet {
   email: string;
@@ -22,13 +23,13 @@ class AuthenticateUserService {
     const user = await userRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorret email password combination.');
+      throw new AppError('Incorret email password combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorret email password combination.');
+      throw new AppError('Incorret email password combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
